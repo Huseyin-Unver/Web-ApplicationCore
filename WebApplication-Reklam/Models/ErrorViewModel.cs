fddd1b6a56d7c8c9be3d41d004b905e4ade3da87
Huseyin-Unver <unverler_02_@hotmail.com>
2024-01-22 22:34:08 +0300

    Add project files.

---
+++ WebApplication-Reklam/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebApplication_Reklam.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
